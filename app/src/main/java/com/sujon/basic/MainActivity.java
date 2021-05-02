4e61ed88e66b338f9b35585f336eeca968231eb1
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-02 21:14:21 +0600

    5.3_Toast - Android Development with Java - Rakibul Islam - Bohubrihi
    5.3_Toast - Android Development with Java - Rakibul Islam - Bohubrihi

--- app/src/main/java/com/sujon/basic/MainActivity.java
+++ app/src/main/java/com/sujon/basic/MainActivity.java
@@ -2,12 +2,13 @@ package com.sujon.basic;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonTutorial;
+    Button buttonTutorial, toastButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +16,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonTutorial = findViewById(R.id.buttonTutorial);
+        toastButton = findViewById(R.id.toastButton);
 
         buttonTutorial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 buttonTutorial.setText("Mutton");
+            }
+        });
+
+        toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Toast Button Clicked",Toast.LENGTH_SHORT).show();
             }
         });
 
