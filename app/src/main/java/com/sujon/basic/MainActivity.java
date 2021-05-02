4efff41ed888b92de64b40a164c8740678afd6a6
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-02 21:03:34 +0600

    5.2_Button - Android Development with Java - Rakibul Islam - Bohubrihi
    5.2_Button - Android Development with Java - Rakibul Islam - Bohubrihi

--- app/src/main/java/com/sujon/basic/MainActivity.java
+++ app/src/main/java/com/sujon/basic/MainActivity.java
@@ -1,13 +1,28 @@
 package com.sujon.basic;
 
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonTutorial;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonTutorial = findViewById(R.id.buttonTutorial);
+
+        buttonTutorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonTutorial.setText("Mutton");
+            }
+        });
+
+
     }
 }
