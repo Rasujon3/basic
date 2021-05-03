534cea5367a64279691bd8f87f8f0c2834afbdc0
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-03 19:48:53 +0600

    5.4_Edit Text - Android Development with Java - Rakibul Islam - Bohubrihi
    5.4_Edit Text - Android Development with Java - Rakibul Islam - Bohubrihi

--- app/src/main/java/com/sujon/basic/MainActivity.java
+++ app/src/main/java/com/sujon/basic/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sujon.basic;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -8,7 +9,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonTutorial, toastButton;
+    Button buttonTutorial, toastButton, editTextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
 
         buttonTutorial = findViewById(R.id.buttonTutorial);
         toastButton = findViewById(R.id.toastButton);
+        editTextButton = findViewById(R.id.editTextButton);
+
+        editTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
 
         buttonTutorial.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/sujon/basic/MainActivity2.java
@@ -0,0 +1,37 @@
+package com.sujon.basic;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    EditText emailEditText;
+    Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        emailEditText = findViewById(R.id.editTextTextEmailAddress);
+        submitButton = findViewById(R.id.button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String userInputValue = emailEditText.getText().toString();
+
+               if (userInputValue.equals("")){
+                   Toast.makeText(MainActivity2.this, "No data inserted", Toast.LENGTH_SHORT).show();
+               } else {
+                   Toast.makeText(MainActivity2.this, userInputValue, Toast.LENGTH_SHORT).show();
+               }
+
+
+            }
+        });
+    }
+}
